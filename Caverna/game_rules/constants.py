2b2b2fd5ac1c8e29082d2704ea19357761e4cda1
D-Lindberg <58619523+D-Lindberg@users.noreply.github.com>
2020-12-06 06:35:51 -0600

    Found a python library that allows dictionaries to be accessed like class attributes vs ['key'] notation. therefore update to requirements.txt and first game rule constant created.

---
+++ Caverna/game_rules/constants.py
@@ -0,0 +1,19 @@
+from attrdict import AttrDict
+
+RESOURCE_TYPES = AttrDict({
+    'Food' : "food",
+    'Wood' : "wood",
+    'Stone' : "stone",
+    'Ore' : "ore",
+    'Gold' : "gold",
+    'Grain' : "grain",
+    'Ruby' : "ruby",
+    'Sheep' : "sheep",
+    'Donkeys' : "donkeys",
+    'Veg' : "veg",
+    'Pigs' : "pigs",
+    'Cows' : "cows",
+    'Begging' : "begging",
+    'Dogs' : "dogs",
+    'ScoreMarker' : "score",
+})

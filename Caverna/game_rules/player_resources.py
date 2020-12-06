e080aa5b51cdb7080d22bc5901bed6a13b1172c9
D-Lindberg <58619523+D-Lindberg@users.noreply.github.com>
2020-12-06 05:55:14 -0600

    Created folder to contain game rules with __init__ and created first game rule component

---
+++ Caverna/game_rules/player_resources.py
@@ -0,0 +1,11 @@
+class PlayerResources:
+    def __init__(self):
+        self.begging_cards = 0
+        self.food = 0
+        self.gold = 0
+        self.ore = 0
+        self.pumkin = 0
+        self.rubies = 0
+        self.stone = 0
+        self.wheat = 0
+        self.wood = 0

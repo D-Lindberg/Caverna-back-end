e3b7349acc0d7a4961f6eb7a7872802c5315c515
D-Lindberg <58619523+D-Lindberg@users.noreply.github.com>
2020-12-06 18:32:14 -0600

    Corrected Spelling Error

--- Caverna/game_rules/player_resources.py
+++ Caverna/game_rules/player_resources.py
@@ -4,7 +4,7 @@ class PlayerResources:
         self.food = 0
         self.gold = 0
         self.ore = 0
-        self.pumkin = 0
+        self.pumpkin = 0
         self.rubies = 0
         self.stone = 0
         self.wheat = 0
